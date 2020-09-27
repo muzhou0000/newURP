af3a3a1b493f853f470703af851ee2ba0e3af9e1
muzhou0000 <55180633+muzhou0000@users.noreply.github.com>
2020-09-27 19:44:41 +0800

    開始畫面/格納

--- LV1/Assets/Scrips/packges.cs
+++ LV1/Assets/Scrips/packges.cs
@@ -6,19 +6,25 @@ using UnityEngine.UI;
 
 public class packges : MonoBehaviour
 {
-    public GameObject UI_Start,BBC;
+    public GameObject UI_Start;
     public int menu;
     public Animator start;
-    void Update()
+    void FixedUpdate()
+    {
+        Invoke("endUIStart_1", 6.5f);
+
+    }
+    void endUIStart_1()
     {
         if (Input.anyKeyDown)//如果按下任意鈕
         {
             start.SetBool("start", true);
             //等17秒後執行下面那個程式
-            Invoke("endUIStart", 15.267f);
+            Invoke("endUIStart_2", 19);
         }
     }
-    void endUIStart()
+
+    void endUIStart_2()
     {
 
         UI_Start.SetActive(false);
