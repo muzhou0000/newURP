ce86f9701ba1c8e250e6d2fc593bdd45ab689103
maoche001 <55180586+maoche001@users.noreply.github.com>
2020-11-06 15:57:45 +0800

    兩關合併

--- LV1/Assets/Scrips/LV2/V_Follow.cs
+++ LV1/Assets/Scrips/LV2/V_Follow.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class V_Follow : MonoBehaviour
 {
@@ -29,6 +31,10 @@ public class V_Follow : MonoBehaviour
         if(other.gameObject.tag=="a")
         {
             Destroy(other.gameObject);
+        }
+        if(other.gameObject.tag=="Player")
+        {
+            SceneManager.LoadScene("LV2");
         }
     }
 }
