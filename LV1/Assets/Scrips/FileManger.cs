731c6f5e84346623d2dc34cbc344813c6857fe5f
muzhou0000 <bono60131@gmail.com>
2020-12-08 01:17:15 +0800

    關得到物品&文件1/10

---
+++ LV1/Assets/Scrips/FileManger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FileManger : MonoBehaviour
+{
+    public GameObject[] File;
+    Color EnbleButton = new Color(116, 255, 0, 225);
+    Color UnenbleButton = new Color(255, 255, 255, 255);
+    int FileCount = 0;
+
+    public void AddFile()
+    {
+        for(int i = 0; i < File.Length; i++)
+        {
+        }
+    }
+}
---
+++ LV1/Assets/Scrips/ItemManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemManager : MonoBehaviour
+{
+    public GameObject[] Item, Tip;
+    Color EnbleButton = new Color(116,255,0,225);
+    Color UnenbleButton = new Color(255, 255, 255, 255);
+    int x=-1;
+    void Update()
+    {
+        
+        if (Input.GetButtonDown("XboxRB")&& Item[x+1].activeSelf)
+        {
+            x += 1;
+        }
+        ChangeColor();
+        if (Tip[x].activeSelf && Input.GetButton("XboxA"))
+        {
+            Item[x].GetComponent<Image>().color = UnenbleButton;
+            Tip[x].SetActive(false);
+            x = -1;
+        }
+    }
+    void ChangeColor()
+    {
+        switch (x)
+        {
+            case 1:
+                Item[x].GetComponent<Image>().color = EnbleButton;
+                Item[x-1].GetComponent<Image>().color = UnenbleButton;
+                Item[x].SetActive(true);
+                Tip[x].SetActive(true);
+                Tip[x-1].SetActive(false);
+                break;
+
+            case 0:
+                Item[x].GetComponent<Image>().color = EnbleButton;
+                Tip[x].SetActive(true);
+                break;
+
+        }
+    }
+}
