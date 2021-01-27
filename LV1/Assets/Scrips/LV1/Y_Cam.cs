0b478434e8786d88644198371c507f8250f6c315
muzhou0000 <bono60131@gmail.com>
2021-01-27 11:03:44 +0800

    0127

--- LV1/Assets/Scrips/LV1/Y_Cam.cs
+++ LV1/Assets/Scrips/LV1/Y_Cam.cs
@@ -19,7 +19,7 @@ public class Y_Cam : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Y.GetBooleanVariable("Y鏡頭判定") == true && Input.GetButton("XboxY"))
+        if (Y.GetBooleanVariable("Y鏡頭判定") == true && Input.GetButton("XboxY") || Y.GetBooleanVariable("Y鏡頭判定") == true && Input.GetKeyDown(KeyCode.Y)) 
         {
             Y.SetBooleanVariable("Y鍵按下", true);
             Cam.SetActive(true);
