a966feafa03358ad4773f4006cce0fbaab05a72e
muzhou0000 <bono60131@gmail.com>
2021-02-01 17:20:18 +0800

    0201

---
+++ LV1/Assets/Scrips/footage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class footage : MonoBehaviour
+{
+    [SerializeField]
+    AudioClip[] clips;
+    AudioSource audiosou;
+
+    private void Awake()
+    {
+        audiosou = GetComponent<AudioSource>();
+    }
+    void Step()
+    {
+        Debug.Log("3");
+        AudioClip clip = GetRandomClip();
+        audiosou.PlayOneShot(clip);
+    }
+    AudioClip GetRandomClip()
+    {
+        return clips[UnityEngine.Random.Range(0, clips.Length)];
+    }
+}
