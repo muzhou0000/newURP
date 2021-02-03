5da35d74be697e5435be6177da61b55674af7a88
maoche001 <maoche001@gmail.com>
2021-02-03 23:00:50 +0800

    0203

---
+++ LV1/Assets/Scrips/LV2/InAndOut_Cam.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InAndOut_Cam : MonoBehaviour
+{
+    public float tempTime;
+    private bool isInView;
+
+    private void Update()
+    {
+        //OnBecameInvisible();
+        if (isInView)
+        {
+            Debug.Log(this.name.ToString() + "這個物體出現在螢幕裡面了");
+            if (tempTime < 1)
+            {
+                tempTime = tempTime + Time.deltaTime;
+            }
+            if (this.GetComponent<Renderer>().material.color.a <= 1)
+            {
+                this.GetComponent<Renderer>().material.color = new Color(
+                this.GetComponent<Renderer>().material.color.r,
+                this.GetComponent<Renderer>().material.color.g,
+                this.GetComponent<Renderer>().material.color.b,
+
+                gameObject.GetComponent<Renderer>().material.color.a + tempTime/50);
+            }
+        }
+    }
+    private void OnBecameVisible()
+    {
+        isInView = true;
+    }
+
+    private void OnBecameInvisible()
+    {
+        isInView = false;
+    }
+
+}
+
+
+
