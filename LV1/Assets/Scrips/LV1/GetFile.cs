c21defbb7e4048d53cb343969f57ba655d70601c
maoche001 <maoche001@gmail.com>
2021-02-16 17:54:23 +0800

    0216

--- LV1/Assets/Scrips/LV1/GetFile.cs
+++ LV1/Assets/Scrips/LV1/GetFile.cs
@@ -4,21 +4,55 @@ using UnityEngine;
 
 public class GetFile : MonoBehaviour
 {
-    public GameObject GetFileTip,RKey;
+    public bool GetFile1;
+    public GameObject GetFileTip,RKey,Box;
+
+    private void Update()
+    {
+        if(RKey==true&&GetFile1==false)
+        {
+            if (Input.GetButtonDown("XboxA") || Input.GetKeyDown(KeyCode.R))
+            {
+                GetFileTip.SetActive(true);
+                RKey.SetActive(false);
+                GetFile1 = true;
+            }
+        }
+        else if (GetFileTip == true && GetFile1 == true)
+        {
+            if (Input.GetButtonDown("XboxA") || Input.GetKeyDown(KeyCode.R))
+            {
+                GetFileTip.SetActive(false);
+                Box.GetComponent<BoxCollider>().enabled = false;
+            }
+        }
+
+        //if (RKey.activeSelf && (Input.GetButtonDown("XboxA") || Input.GetKeyDown(KeyCode.R)))
+        //{
+        //    GetFileTip.SetActive(true);
+        //    RKey.SetActive(false);
+        //    GetFile1 = true;
+        //}
+        //else if (GetFileTip.activeSelf && (Input.GetButtonDown("XboxA") || Input.GetKeyDown(KeyCode.R)))
+        //{
+        //    GetFileTip.SetActive(false);
+        //    if (GetFile1)
+        //    {
+        //        Box.GetComponent<BoxCollider>().enabled = false;
+        //    }
+        //}
+    }
     public void OnTriggerEnter(Collider other)
     {
         RKey.SetActive(true);
-        
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        RKey.SetActive(false);
     }
     private void OnTriggerStay(Collider other)
     {
-        if (RKey.activeSelf && (Input.GetButtonDown("XboxA") || Input.GetKeyDown(KeyCode.R)))
-        {
-            RKey.SetActive(false);
-            GetFileTip.SetActive(true);
-        }else if(GetFileTip.activeSelf && (Input.GetButtonDown("XboxA") || Input.GetKeyDown(KeyCode.R)))
-        {
-            GetFileTip.SetActive(false);
-        }
+        
     }
 }
