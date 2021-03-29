356e8786c0851f56f682b5ad680dcf4226c21988
muzhou0000 <bono60131@gmail.com>
2021-03-30 02:32:25 +0800

    幫文件上色

---
+++ LV1/Assets/Scrips/LV1/objectMusic.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectMusic : MonoBehaviour
+{
+    AudioSource audioSou;
+    //public AudioClip brock;
+    [Tooltip("Sound effect clip to play")]
+    [SerializeField]
+    protected AudioClip[] soundClip;
+    [SerializeField]
+    protected AudioClip[] PoilClip;
+
+
+    private void Start()
+    {
+        audioSou = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Materials")
+        {
+            int rand = Random.Range(0, soundClip.Length);
+            if (soundClip == null)
+            {
+                return;
+            }
+            audioSou.PlayOneShot(soundClip[rand]);
+        }
+        if (other.gameObject.tag == "b")
+        {
+            int rand_button = Random.Range(0, PoilClip.Length);
+            if (PoilClip == null)
+            {
+                return;
+            }
+            audioSou.PlayOneShot(PoilClip[rand_button]);
+        }
+    }
+
+
+}
