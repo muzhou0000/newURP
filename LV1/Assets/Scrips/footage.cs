97532befe8db9e58e8c57ba12e6b49f273409728
muzhou0000 <55180633+muzhou0000@users.noreply.github.com>
2021-04-23 22:14:36 +0800

    哭啊

--- LV1/Assets/Scrips/footage.cs
+++ LV1/Assets/Scrips/footage.cs
@@ -7,6 +7,9 @@ public class footage : MonoBehaviour
 {
     [SerializeField]
     protected AudioClip[] clips;
+    [SerializeField]
+    protected AudioClip[] ladder_clips;
+
     AudioSource audiosou;
 
     private void Awake()
@@ -22,6 +25,16 @@ public class footage : MonoBehaviour
             return;
         }
         audiosou.PlayOneShot(clips[rand]);
+    }
+    void ladder()
+    {
+        audiosou.volume = 0.152f;
+        int ladder_rand = Random.Range(0, ladder_clips.Length);
+        if (ladder_clips == null)
+        {
+            return;
+        }
+        audiosou.PlayOneShot(ladder_clips[ladder_rand]);
     }
     //AudioClip GetRandomClip()
     //{
